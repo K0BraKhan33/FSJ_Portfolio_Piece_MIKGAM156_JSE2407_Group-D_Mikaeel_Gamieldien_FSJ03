a44104d0605034c1b951a438fec827c3292cb12e
K0BraKhan33 <mikaeel.gamieldien@gmail.com>
2024-10-05 15:26:18 +0200

    modified:   food-com-store/next.config.mjs

--- food-com-store/next.config.mjs
+++ food-com-store/next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['cdn.dummyjson.com'], // Add the allowed hostname here
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ['cdn.dummyjson.com'], // Add the allowed hostname here
+  },
+  telemetry: false, // Disable telemetry
+};
+
+export default nextConfig;
